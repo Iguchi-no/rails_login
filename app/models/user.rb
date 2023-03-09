7cf3865d255806d599652f0420f853f0a5c6ce26
Iguchi-no <iguchipro1029@gmail.com>
2023-03-10 01:29:34 +0900

    edit User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+    # emailを小文字に統一
+    before_save { self.email = email.downcase }
+    # emailのフォーマット指定
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # emailのバリデーション
+    validates :email,
+        presence: true,
+        length: { maximun: 255 },
+        format: { with: VALID_EMAIL_REGEX },
+        uniqueness: { case_sensitive: false }
+    # passwordのバリデーション
+    validates :password,
+        presence: true,
+        length: { minimun: 6 }
+    has_secure_password
 end
