d4718148d55bc510b4d09709bb5d29fd79e69091
Iguchi-no <iguchipro1029@gmail.com>
2023-03-10 12:33:40 +0900

    add views and fix spell
    maximun -> maximum

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
     # emailのフォーマット指定
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     # emailのバリデーション
-    validates :email,
-        presence: true,
-        length: { maximun: 255 },
+    validates :email, 
+        presence: true, 
+        length: { maximum: 255 },
         format: { with: VALID_EMAIL_REGEX },
         uniqueness: { case_sensitive: false }
     # passwordのバリデーション
     validates :password,
         presence: true,
-        length: { minimun: 6 }
+        length: { minimum: 6 }
     has_secure_password
 end
